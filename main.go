0fbc73f33f253573314d4b6cfed70b0636b09f58
Ritesh Yadav <53623244+DARK-art108@users.noreply.github.com>
2020-07-20 09:49:26 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/anaskhan96/soup"
+	tbot "github.com/go-telegram-bot-api/telegram-bot-api"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var bot *tbot.BotAPI
+
+func ButtonLinks(ID int64, ButtonText string, ButtonUrl string, MessageText string) {
+	var button = tbot.NewInlineKeyboardMarkup(
+		tbot.NewInlineKeyboardRow(
+			tbot.NewInlineKeyboardButtonURL(ButtonText, ButtonUrl),
+		),
+	)
+	msg := tbot.NewMessage(ID, MessageText)
+	msg.ReplyMarkup = button
+	bot.Send(msg)
+}
+
+//scraping xkcd strip URL from its website with the help of a random generated integer and then sending it as a photo using NewPhotoShare Telegram API method.
+func xkcd(ID int64) {
+	rand.Seed(time.Now().UnixNano())
+	min := 100
+	max := 2000
+	randomnum := (rand.Intn(max-min+1) + min)
+	xkcdurl := "https://xkcd.com/" + strconv.Itoa(randomnum)
+	resp, err := soup.Get(xkcdurl)
+	if err == nil {
+		doc := soup.HTMLParse(resp)
+		links := doc.Find("div", "id", "comic").FindAll("img")
+		for _, link := range links {
+			imglink := link.Attrs()["src"]
+			fullurl := "https:" + imglink
+			fmt.Println(fullurl)
+			bot.Send(tbot.NewPhotoShare(ID, fullurl))
+		}
+	} else {
+		fullurl := "https://imgs.xkcd.com/comics/operating_systems.png"
+		bot.Send(tbot.NewPhotoShare(ID, fullurl))
+	}
+}
+
+func help(ID int64) {
+	msg := ` Use one of the following commands to :
+	/github - Get a link to OSDC's Github page.
+	/telegram - Get an invite link for OSDC's Telegram Group.
+	/twitter - Get the link of OSDC's twitter account.
+	/website - Get the link of the official website of OSDC.
+	/blog - Get the link of the OSDC blog.
+	/irc - Find us on IRC.
+	/xkcd - Get a xkcd comic for you.
+	/dlmeetups - Get the list of upcoming meetups of Delhi/NCR communities.
+	/addmeetup* - Add the details of next OSDC Meetup.` + "\n" + `Format : /addmeetup <Title> <DD/MM/YYYY Hr:Min> <Venue>
+	/nextmeetup - Get the details of next OSDC Meetup
+	To contribute to|modify this bot : https://github.com/vaibhavk/osdc-bots
+	* - Only for channel admins.
+	`
+	bot.Send(tbot.NewMessage(ID, msg))
+}
+
+//list of meetup groups urlnames,keeps sending one by one to getmeetups() function in meetup.go
+func dlmeetups(ID int64) {
+	finallist = ""
+	urlnames := []string{"ilugdelhi", "pydelhi", "GDGNewDelhi", "gdgcloudnd", "Paytm-Build-for-India", "jslovers", "Gurgaon-Go-Meetup", "Mozilla_Delhi", "PyDataDelhi", "React-Delhi-NCR", "OWASP-Delhi-NCR-Chapter"}
+	for _, each := range urlnames {
+		url := "http://api.meetup.com/" + each + "/events"
+		getMeetups(url)
+	}
+	bot.Send(tbot.NewMessage(ID, finallist))
+}
+
+func welcome(user tbot.User, ID int64) {
+	User := fmt.Sprintf("[%v](tg://user?id=%v)", user.FirstName, user.ID)
+	var botSlice = make([]string, 0)
+	botSlice = append(botSlice,
+		"helping the hackers in here.",
+		"a bot made by the geeks for the geeks.",
+		"an Autobot on Earth to promote open source.",
+		"a distant cousin of the mars rover.",
+		"a friendly bot written in Golang.",
+	)
+	var quesSlice = make([]string, 0)
+	quesSlice = append(quesSlice,
+		"which language do you work with?",
+		"what do you want to learn?",
+		"what is your current operating system?",
+		"if you are new to open source.",
+		"which technology fascinates you the most?",
+		"what have you been exploring recently?",
+	)
+
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := len(botSlice) - 1
+	randomNum1 := (rand.Intn(max-min+1) + min)
+	randomNum2 := (rand.Intn(max-min+1) + min)
+	var welcomeMessage1 = fmt.Sprintf(botSlice[randomNum1])
+	var welcomeMessage2 = fmt.Sprintf(quesSlice[randomNum2])
+	reply := tbot.NewMessage(ID, "Welcome "+User+", I am the OSDC-bot, "+welcomeMessage1+
+		" Please introduce yourself. To start with, you can tell us "+welcomeMessage2)
+	reply.ParseMode = "markdown"
+	bot.Send(reply)
+}
+
+//extracts the details of the user who sent the message to check whether the user is creator/admin. Returns true in this case else false.
+func memberdetails(ID int64, userid int) bool {
+	response, _ := bot.GetChatMember(tbot.ChatConfigWithUser{
+		ChatID: ID,
+		UserID: userid,
+	})
+	if response.IsCreator() || response.IsAdministrator() {
+		return true
+	} else {
+		return false
+	}
+}
+
+func kickUser(user int, ID int64) {
+	bot.KickChatMember(tbot.KickChatMemberConfig{
+		ChatMemberConfig: tbot.ChatMemberConfig{
+			ChatID: ID,
+			UserID: user,
+		},
+		UntilDate: time.Now().Add(time.Hour * 24).Unix(),
+	})
+}
+
+func main() {
+	var err error
+	bot, err = tbot.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		fmt.Println("error in auth")
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	u := tbot.NewUpdate(0)
+	u.Timeout = 60
+	updates, _ := bot.GetUpdatesChan(u)
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		ID := update.Message.Chat.ID
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+
+			case "start":
+				ButtonLinks(ID, "Click Here", "https://t.me/jiitosdc", "Hello , I'm OSDC Bot, Use /help to know more. To join OSDC group:")
+			case "help":
+				help(ID)
+			case "github":
+				ButtonLinks(ID, "Github", "https://github.com/osdc", "Checkout our OSDC projects and feel free to contribute!")
+			case "telegram":
+				ButtonLinks(ID, "Click Here", "https://t.me/jiitosdc", "Join our telegram group:")
+			case "twitter":
+				ButtonLinks(ID, "Click Here", "https://twitter.com/osdcjiit", "Check us out on twitter")
+			case "website":
+				ButtonLinks(ID, "Click Here", "https://osdc.netlify.com", "Website:")
+			case "blog":
+				ButtonLinks(ID, "Click Here", "https://osdcblog.netlify.com/", "Blogs written by the folks at the Open Source Developers' Club who live in and around JIIT, Noida, India.")
+			case "irc":
+				ButtonLinks(ID, "Click Here", "https://github.com/osdc/community-committee/wiki/IRC", "Join us on IRC server of Freenode at #jiit-lug. To get started refer our IRC wiki-")
+			case "xkcd":
+				xkcd(ID)
+			case "dlmeetups":
+				dlmeetups(ID)
+			case "addmeetup":
+				check := memberdetails(ID, update.Message.From.ID)
+				if check == true {
+					addmeetup(ID, update.Message.Text, *client)
+				} else {
+					bot.Send(tbot.NewMessage(ID, "Sorry, only admins can add the details of next meetup."))
+				}
+			case "nextmeetup":
+				nextmeetup(ID, *client)
+			case "save":
+				check := memberdetails(ID, update.Message.From.ID)
+				if check == true {
+					savenote(ID, update.Message.Text, *client)
+				} else {
+					bot.Send(tbot.NewMessage(ID, "Sorry, only admins can add the details of next meetup."))
+				}
+			case "notes":
+				fetchallnotes(ID, *client)
+			case "deletenote":
+				deletenote(ID, update.Message.Text, *client)
+			case "fetchnote":
+				fetchnote(ID, update.Message.Text, *client)
+			default:
+				bot.Send(tbot.NewMessage(ID, "I don't know that command"))
+			}
+		}
+		if update.Message.NewChatMembers != nil {
+			for _, user := range *(update.Message.NewChatMembers) {
+				if user.IsBot && user.UserName != "osdcbot" {
+					go kickUser(user.ID, ID)
+				} else {
+					go welcome(user, ID)
+				}
+			}
+		}
+	}
+}
---
+++ meetup.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	tbot "github.com/go-telegram-bot-api/telegram-bot-api"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var finallist string //variable to store final list of meetups
+
+//-------structs to parse json data from Meetup API
+type venue struct {
+	Venue string `json:"name"`
+}
+type group struct {
+	GroupName string `json:"name"`
+}
+type meetuplist struct {
+	Name  string `json:"name"`
+	Venue venue  `json: "venue"`
+	Date  string `json:"local_date"`
+	Time  string `json:"local_time"`
+	Group group  `json: "group"`
+	Link  string `json:"link"`
+}
+
+//struct to keep track of OSDC meetups using a JSON file
+type meetupdata struct {
+	Name  string
+	Date  time.Time
+	Venue string
+}
+
+//fetching details of meetup of the group's url from Meetup API
+func getMeetups(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var meetups []meetuplist
+	json.Unmarshal([]byte(body), &meetups)
+	if (len(meetups)) > 0 {
+		finallist = finallist + "Title -" + "\t" + meetups[0].Name + "\n" + "Date -" + "\t" + meetups[0].Date + "\n" + "Link -" + "\t" + meetups[0].Link + "\n\n"
+	}
+}
+
+//Slicing the arguments (title & date of next meetup) from message text and writing them to a json file.
+func addmeetup(ID int64, msgtext string, client mongo.Client) {
+	// Get a handle for your collection
+	collection := client.Database("test").Collection("meetups")
+
+	args := strings.Fields(msgtext)
+	if len(args) == 5 {
+		//separating the date and hour arguments so that we can use the
+		// dd/mm/yyyy and hh:min format
+		separatedDateStr := strings.Split(args[2], "/")
+		separatedHourStr := strings.Split(args[3], ":")
+
+		//converting the arguments into integers to pass them to time.Date
+		day, err := strconv.Atoi(separatedDateStr[0])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		month, err := strconv.Atoi(separatedDateStr[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		year, err := strconv.Atoi(separatedDateStr[2])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		hour, err := strconv.Atoi(separatedHourStr[0])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		minutes, err := strconv.Atoi(separatedHourStr[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		data := meetupdata{
+			Name:  args[1],
+			Date:  time.Date(year, time.Month(month), day, hour, minutes, 0, 0, time.Local),
+			Venue: args[4],
+		}
+		_, err = collection.DeleteMany(context.TODO(), bson.D{{}})
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = collection.InsertOne(context.TODO(), data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bot.Send(tbot.NewMessage(ID, "Meetup added successfully."))
+
+		//making a formatted string from time to pass it to s.At(). This will
+		//specify the time at which the reminder is sent. Currently 2 hours
+		//before the meetup
+		remindTime := fmt.Sprintf("%d:%d", hour-2, minutes)
+
+		//making a new scheduler
+		s1 := gocron.NewScheduler(time.Local)
+
+		//assigning the scheduler a task and then starting it
+		s1.Every(1).Day().At(remindTime).Do(reminder, ID, client, s1)
+		s1.Start()
+	} else {
+		bot.Send(tbot.NewMessage(ID, "Please provide the details of meetup"+
+			" in this format - /addmeetup <Title> <DD/MM/YY Hr:Min> <Venue> "+
+			"\nTIME IN 24Hr FORMAT"))
+	}
+}
+
+//fetching the details (Next Meetup Title & Date) from the JSON file
+func nextmeetup(ID int64, client mongo.Client) {
+	collection := client.Database("test").Collection("meetups")
+	var data meetupdata
+	err := collection.FindOne(context.TODO(), bson.M{}).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//the string inside Format method is a sample string to specify the display
+	//format of meetupTimeString
+	timeString := data.Date.Local().Format("Mon _2 Jan 2006")
+	nxtMeetupData := "Details of next OSDC Meetup :" + "\n" + "Title -" + "\t" +
+		data.Name + "\n" + "Date -" + "\t" + timeString + "\n" + "Time -" + "\t" +
+		data.Date.Local().Format("15:04") + "\n" + "Venue -" + "\t" + data.Venue
+	bot.Send(tbot.NewMessage(ID, nxtMeetupData))
+}
+
+func reminder(ID int64, client mongo.Client, s1 *gocron.Scheduler) {
+	collection := client.Database("test").Collection("meetups")
+	var data meetupdata
+	err := collection.FindOne(context.TODO(), bson.M{}).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//reminder will be sent only if the the meetup is today
+	if time.Now().Local().Day() == data.Date.Day() {
+		//the string inside Format method is a sample string to specify the display
+		//format of meetupTimeString
+		timeString := data.Date.Local().Format("Mon _2 Jan 2006")
+		nxtMeetupData := "MEETUP REMINDER!" + "\n" + "Title -" + "\t" +
+			data.Name + "\n" + "Date -" + "\t" + timeString + "\n" + "Time -" +
+			"\t" + data.Date.Local().Format("15:04") + "\n" +
+			"Venue -" + "\t" + data.Venue
+		if !time.Now().Local().Before(data.Date) {
+			//stops the scheduler when meetup is done
+			s1.Clear()
+		} else {
+			bot.Send(tbot.NewMessage(ID, nxtMeetupData))
+		}
+	}
+}
